41bd95d146fed3cc9e03f94e46724a5ae85a7023
zino <thiszino@gmail.com>
2023-01-31 11:01:35 +0900

    frist commit

---
+++ EmployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package edu.kh.emp.model.dao;
+
+// DAO(Data Access Object, 데이터 접근 객체)
+// -> 데이터베이스에 접근(연결)하는 객체
+// --> JDBC 코드 작성
+public class EmployeeDAO {
+
+
+	
+}
+
+
+
+
+
+
+
+
---
+++ EmployeeManagement/src/edu/kh/emp/view/EmployeeView.java
@@ -0,0 +1,195 @@
+package edu.kh.emp.view;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.List;
+
+//import edu.kh.emp.model.dao.EmployeeDAO;
+import edu.kh.emp.model.vo.Employee;
+
+// 화면용 클래스( 입력(Scanner) / 출력(print()) )
+public class EmployeeView {
+
+	private Scanner sc = new Scanner(System.in);
+	
+	// DAO 객체 생성
+	//private EmployeeDAO dao = new EmployeeDAO();
+	
+	
+	
+	// 메인 메뉴
+	public void displayMenu() {
+		
+		int input = 0;
+		
+		do {
+			try {
+				System.out.println("---------------------------------------------------------");
+				System.out.println("----- 사원 관리 프로그램 -----");
+				System.out.println("1. 새로운 사원 정보 추가");
+				System.out.println("2. 전체 사원 정보 조회");
+				System.out.println("3. 사번이 일치하는 사원 정보 조회");
+				System.out.println("4. 사번이 일치하는 사원 정보 수정");
+				System.out.println("5. 사번이 일치하는 사원 정보 삭제");
+				
+				System.out.println("6. 입력 받은 부서와 일치하는 모든 사원 정보 조회");
+				// selectDeptEmp()
+				
+				System.out.println("7. 입력 받은 급여 이상을 받는 모든 사원 정보 조회");
+				// selectSalaryEmp()
+				
+				System.out.println("8. 부서별 급여 합 전체 조회");
+				// selectDeptTotalSalary()
+				// DB 조회 결과를 HashMap<String, Integer>에 옮겨 담아서 반환
+				// 부서코드, 급여 합 조회
+				
+				System.out.println("9. 주민등록번호가 일치하는 사원 정보 조회");
+				
+				System.out.println("10. 직급별 급여 평균 조회");
+				// selectJobAvgSalary()
+				// DB 조회 결과를 HashMap<String, Double>에 옮겨 담아서 반환 
+				// 직급명, 급여 평균(소수점 첫째자리) 조회
+				
+				
+				System.out.println("0. 프로그램 종료");
+				
+				System.out.print("메뉴 선택 >> ");
+				input = sc.nextInt();
+				sc.nextLine(); //  추가!
+				
+				
+				System.out.println();				
+				
+				
+				switch(input) {
+				case 1:  insertEmployee();   break;
+				case 2:  selectAll();  break;
+				case 3:  selectEmpId();   break;
+				case 4:  updateEmployee();   break;
+				case 5:  deleteEmployee();   break;
+				case 6:  selectDeptEmp();   break;
+				case 7:  selectSalaryEmp();   break;
+				case 8:  selectDeptTotalSalary();   break;
+				case 9:  selectEmpNo();   break;
+				case 10: selectJobAvgSalary();   break;
+				
+				case 0:  System.out.println("프로그램을 종료합니다...");   break;
+				default: System.out.println("메뉴에 존재하는 번호만 입력하세요.");
+				}
+				
+				
+			}catch(InputMismatchException e) {
+				System.out.println("정수만 입력해주세요.");
+				input = -1; // 반복문 첫 번째 바퀴에서 잘못 입력하면 종료되는 상황을 방지
+				sc.nextLine(); // 입력 버퍼에 남아있는 잘못 입력된 문자열 제거해서
+							   // 무한 반복 방지
+			}
+			
+		}while(input != 0);
+		
+	}
+	
+	
+	/**
+	 * 전체 사원 정보 조회
+	 */
+	public void selectAll() {
+		
+	}
+	
+	
+	/** 전달받은 사원 List 모두 출력
+	 * @param empList
+	 */
+	public void printAll(List<Employee> empList) {
+		return;
+	}
+	
+	
+	/**
+	 * 사번이 일치하는 사원 정보 조회
+	 */
+	public void selectEmpId() {
+		
+	}
+	
+	
+	/** 사번을 입력 받아 반환하는 메서드
+	 * @return empId
+	 */
+	public int inputEmpId() {
+		return 0;
+	}
+	
+	
+	/** 사원 1명 정보 출력
+	 * @param emp
+	 */
+	public void printOne(Employee emp) {
+		
+	}
+	
+	
+	/**
+	 * 주민등록번호가 일치하는 사원 정보 조회
+	 */
+	public void selectEmpNo() {
+
+	}
+	
+	
+	/**
+	 * 사원 정보 추가
+	 */
+	public void insertEmployee() {
+		
+	}
+	
+	
+	/**
+	 * 사번이 일치하는 사원 정보 수정(이메일, 전화번호, 급여)
+	 */
+	public void updateEmployee() {
+		
+	}
+	
+	/**
+	 * 사번이 일치하는 사원 정보 삭제
+	 */
+	public void deleteEmployee() {
+		
+	}
+	
+	
+	/**
+	 * 입력 받은 부서와 일치하는 모든 사원 정보 조회
+	 */
+	public void selectDeptEmp() {
+
+	}
+	
+	/**
+	 * 입력 받은 급여 이상을 받는 모든 사원 정보 조회
+	 */
+	public void selectSalaryEmp() {
+		
+	}
+	
+	/**
+	 * 부서별 급여 합 전체 조회
+	 */
+	public void selectDeptTotalSalary() {
+		
+	}
+	
+	/**
+	 * 직급별 급여 평균 조회
+	 */
+	public void selectJobAvgSalary() {
+		
+		
+	}
+	
+	
+	
+}
